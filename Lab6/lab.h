97ce946b5b950ec18a5eb8bb518e1028c39d314d
zSacred <r.rashchupkin@yandex.ru>
2020-12-25 11:33:46 +0300

    Добавьте файлы проекта.

---
+++ Lab6/lab.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+
+std::vector<int>::iterator Find(std::vector<int>::iterator first, std::vector<int>::iterator last, int el);
---
+++ Lab6/lab06.cpp
@@ -0,0 +1,12 @@
+// Lab6.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <vector>
+
+
+int main()
+{
+    return 0;
+}
+
